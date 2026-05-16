0d35c2408e07a925800dd970dff3f3123843111a
Ahmed Afifi <ahmed.khaled.afifi22@gmail.com>
2026-05-16 18:20:38 +0300

    added comments [skip ci]

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import HelpArea from './HelpArea';
-
+// added a comment to this file to skip triggering the workflow
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
 
